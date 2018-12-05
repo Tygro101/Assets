1f839e51e9cdd6dac0ca1522a9ce56ef10db155f
Ran Tayeb <Tayebran@gmail.com>
2018-12-05 20:06:17 +0200

    Initial commit

---
+++ Scripts/Rotation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Rotation : MonoBehaviour {
+    Vector3 mPrevPos = Vector3.zero;
+    Vector3 mPosDelta = Vector3.zero;
+
+    private void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            mPosDelta = Input.mousePosition - mPrevPos;
+            if(Vector3.Dot(transform.up, Vector3.up) >= 0)
+            {
+                transform.Rotate(transform.up, -Vector3.Dot(mPosDelta, Camera.main.transform.right), Space.World);
+            }
+            else
+            {
+                transform.Rotate(transform.up, -Vector3.Dot(mPosDelta, Camera.main.transform.right), Space.World);
+            }
+
+        }
+        mPrevPos = Input.mousePosition;
+    }
+}
